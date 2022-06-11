3e37d3fee2723144fa059b5c2f5a7e14367be3a8
John Eriko <boncodin.johneriko@gmail.com>
2022-06-11 19:22:53 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,65 @@
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    navbar.classList.toggle("open-menu");
+    menu.classList.toggle("move");
+};
+window.onscroll = () =>{
+    navbar.classList.remove("open-menu");
+    menu.classList.remove("move");
+};
+
+//Email JS
+function validate(){
+    let name = document.querySelector(".name")
+    let email = document.querySelector(".email")
+    let msg = document.querySelector(".message")
+    let sendBtn = document.querySelector(".send-btn")
+
+    sendBtn.addEventListener('click', (e) =>{
+        e.preventDefault();
+        if(name.value== "" || email.value == "" || msg.value == ""){
+            emptyerror();
+        } else{
+            sendmail(name.value, email.value, msg.value);
+            success();
+        }
+    });
+}
+validate();
+
+function  emptyerror(){
+    swal({
+        title: "Error!",
+        text: "Fields cannot be empty",
+        icon: "error",
+      });
+}
+function sendmail(name,email,msg){
+    emailjs.send("service_znhj89b","template_zz7xu6c",{
+        to_name: "John Eriko",
+        from_name: email,
+        message: msg,
+        });
+}
+function success(){
+    swal({
+        title: "Success",
+        text: "Email Sent Successfully",
+        icon: "success",
+      });
+}
+//Header Background Chnage On Scroll
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("header-active", window.scrollY > 0);
+});
+
+//Scroll Top
+let scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+    scrollTop.classList.toggle("scroll-active", window.scrollY >= 400);
+});
